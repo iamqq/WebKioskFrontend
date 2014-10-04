aed404e872383054dd95e47cb245f71efbfe7b04
iamqq <sergejr@gmail.com>
2014-10-04 09:48:45 +0600

    zzz
    zzz

--- js/app.js
+++ js/app.js
@@ -3,16 +3,18 @@
   var app = angular.module('kiosk', ['ngAnimate','ngRoute']);
   app.controller('Proximity',['$scope','$interval', function($scope,$interval){
     $scope.proximity = prx;
-
+    $scope.prxtimer = 0;
       var stop;
       $scope.clearproxmt = function() {
         if ( angular.isDefined(stop) ) return;
 
         stop = $interval(function() {
-          if ($scope.proximity.code.length>0){
-            $scope.proximity.code = $scope.proximity.code.substring(0,$scope.proximity.code.length-1);
-          }
-        }, 200);
+          if ($scope.prxtimer>0){
+            $scope.prxtimer--;
+          } else {
+           $scope.proximity.code = "";
+          } 
+        }, 100);
     };
     $scope.clearproxmt();
   }]);
@@ -24,5 +26,6 @@ function changeProximityCode(code) {
     var controllerElement = document.getElementById('info');
     var controllerScope = angular.element(controllerElement).scope();
     controllerScope.proximity.code = code;
+    controllerScope.prxtimer = 100;
     appScope.$apply(function() {});
 }
